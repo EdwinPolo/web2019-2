878471fd109513c5ecb3ce604cebad94a8aa4ac5
ESTUDIANTE <estudiante@ul.edu.co>
2019-11-07 19:59:58 -0500

    pointing to API

--- src/app/pages/biblioteca/autores/autores.component.ts
+++ src/app/pages/biblioteca/autores/autores.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { AuthorService } from 'src/app/services/author.service';
 
 @Component({
   selector: 'app-autores',
@@ -9,26 +10,34 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
 export class AutoresComponent implements OnInit {
 
   public autoresForm: FormGroup;
+  public authors: any 
 
-  public titulo = "Formulario de autores"
+  public titulo = "Formulario de autores";
   public nombre : String;
   public apellido : String;
   public fecha : String;
   public nacionalidad : String;
 
-  constructor(protected fb: FormBuilder) { 
+  constructor(protected fb: FormBuilder, protected service: AuthorService) { 
     this.createForm()
   }
 
   ngOnInit() {
+    this.service.getAuthors().subscribe(data => {
+      this.authors = data
+    })
+  }
+
+  saveAuthor() {
+    this.service.postAuthor(this.autoresForm.value).subscribe(data => alert("listo"))
   }
 
   createForm() {
     this.autoresForm = this.fb.group({
-      code: ['test', Validators.required],
+      code: ['', Validators.required],
       name: ['', Validators.required],
       lastname: ['', Validators.required],
-      bd_year : ['test', Validators.required], //Fecha de Nacimiento
+      bd_year : ['', Validators.required], //Fecha de Nacimiento
       death_year: ['', Validators.required], //Fecha de muerte
       bd_place: ['', Validators.required] //Lugar de Nacimiento
     });
